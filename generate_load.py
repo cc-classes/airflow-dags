e9d6c18e2bb9f2442dc21855d300f82e6c66e6f7
Eric Greene <eric@t4d.io>
2025-03-07 07:14:42 -0500

    add generate load dag

--- generate_load.py
+++ generate_load.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 from airflow import DAG
@@ -13,6 +14,12 @@ def long_task(size=1):
     print(f"<<<<<<<<< Finished long task ({size}) >>>>>>>>>>")
 
 
+def long_task_time(delay=1):
+    print(f"<<<<<<<<< Starting long task ({delay}s) >>>>>>>>>>")
+    time.sleep(delay)
+    print(f"<<<<<<<<< Finished long task ({delay}s) >>>>>>>>>>")
+
+
 with DAG(
     "generate_load",
     start_date=datetime(2025, 1, 1),
@@ -20,13 +27,25 @@ with DAG(
     catchup=False,
 ) as dag:
     num_of_tasks = 1000
-    num_of_loops = 500000000
+
+    # num_of_loops = 500000000
+
+    # tasks = [
+    #     PythonOperator(
+    #         task_id=f"task{i}",
+    #         python_callable=long_task,
+    #         op_args=[num_of_loops],
+    #     )
+    #     for i in range(num_of_tasks)
+    # ]
+
+    delay = 30
 
     tasks = [
         PythonOperator(
             task_id=f"task{i}",
-            python_callable=long_task,
-            op_args=[num_of_loops],
+            python_callable=long_task_time,
+            op_args=[delay],
         )
         for i in range(num_of_tasks)
     ]
